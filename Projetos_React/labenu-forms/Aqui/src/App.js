3763c5e498cf717c4c30d6909615199c717f6821
Pereira-Araujo <araujo_ir@yahoo.com>
2021-04-04 09:09:07 -0300

    Utilização do App para exibir a página formulário

--- Projetos_React/labenu-forms/Aqui/src/App.js
+++ Projetos_React/labenu-forms/Aqui/src/App.js
@@ -1,76 +1,9 @@
-import React from 'react'
-import styled from 'styled-components'
-import Etapa1 from './Pages/Etapa1'
-import Etapa2 from './Pages/Etapa2'
-import Etapa3 from './Pages/Etapa3'
-import Final from './Pages/Final'
+import React from "react"
+import Formulario from "./Pages/Formulario/Formulario"
 
-
-const Container = styled.main`
- text-align:center;
-  background: linear-gradient(
-    90deg,
-    hsla(29, 92%, 70%, 1) 0%,
-    hsla(0, 87%, 73%, 1) 100%
-  );
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 100vh;
-`
-
-const FormStyle = styled.section`
-  border-radius: 4%;
-  width: 310px;
-  height: 400px;
-  background: white;
-  box-shadow: -4px 14px 15px 0px rgba(0, 0, 0, 0.72);
-`
-const ButtonStyle = styled.button`
-  margin-top: 10px;
-`
-
-class App extends React.Component {
-  state = {
-    etapa: 1,
-  }
-
-  onClickProximaEtapa = () => {
-    this.setState({
-      etapa: this.state.etapa + 1,
-    })
-  }
-
-  render() {
-    const renderizaEtapa = () => {
-      switch (this.state.etapa) {
-        case 1:
-          return <Etapa1 />
-        case 2:
-          return <Etapa2 />
-        case 3:
-          return <Etapa3 />
-        case 4:
-          return <Final />
-        default:
-          return 'ERRO'
-      }
-    }
-
-    return (
-      <Container>
-        <FormStyle>
-          {renderizaEtapa()}
-          {this.state.etapa !== 4 && (
-            <ButtonStyle onClick={this.onClickProximaEtapa}>
-              Proxima etapa
-            </ButtonStyle>
-          )}
-        </FormStyle>
-      </Container>
-    )
-  }
+function App(){
+  return(
+<Formulario/>
+  )
 }
-
-export default App
+export default App;
