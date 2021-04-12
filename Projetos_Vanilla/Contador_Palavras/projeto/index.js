7a624b086605f8aa82a35fa2519a89e2d0e694ce
Pereira-Araujo <araujo_ir@yahoo.com>
2021-04-12 12:11:57 -0300

    Formatação

--- Projetos_Vanilla/Contador_Palavras/projeto/index.js
+++ Projetos_Vanilla/Contador_Palavras/projeto/index.js
@@ -1,12 +1,12 @@
 const contar = () =>{
-let texto = document.querySelector('.texto').value
-let quantidade = document.querySelector('.quantidade')
+let texto = document.querySelector('.card__texto').value
+let quantidade = document.querySelector('.card__contador')
 let index = 0 
 const palavras = []
 let textoSeparado = texto.split(/\s+/)
 
 for(index;index<textoSeparado.length;index++){
-  if(textoSeparado[index] !==""){
+  if(textoSeparado[index] !=="" ){
     palavras.push(textoSeparado[index])
   }
 }
