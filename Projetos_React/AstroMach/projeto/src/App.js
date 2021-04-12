da65c1148bf0b3579108c08c41b07f1c7c6509fa
Pereira-Araujo <araujo_ir@yahoo.com>
2021-04-12 16:29:15 -0300

    :tada: Criação

---
+++ Projetos_React/AstroMach/projeto/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Interface from "./Componentes/Interface/Interface";
+
+
+const App = () => {
+  return (
+    <div>
+      <Interface />
+    </div>
+
+  );
+};
+
+export default App;
