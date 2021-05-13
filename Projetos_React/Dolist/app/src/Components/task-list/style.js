81e63fe44dccfe1a9cad746087072b44bc4bd8f5
Pereira-Araujo <araujo_ir@yahoo.com>
2021-05-13 00:02:56 -0300

    Ajustes de estilo

--- Projetos_React/Dolist/app/src/Components/task-list/style.js
+++ Projetos_React/Dolist/app/src/Components/task-list/style.js
@@ -1,28 +1,50 @@
 import styled from "styled-components";
 
-export const Container = styled.main`
+export const ContainerCard = styled.main`
 
   font-family: sans-serif;
   text-align: center;
   display: flex;
   flex-direction: column;
+  justify-content:center;
   align-items: center;
+  height:100vh;
+  background:aquamarine;
 
 `
 
-export const TaskCard = styled.ul`
+export const Card = styled.section`
+width: 14rem;
+height: 16rem;
+border-radius:10%;
+box-shadow: -6px 10px 5px 0px rgba(0,0,0,0.75);
+
+padding:2rem;
+background:white;
+display:flex;
+flex-direction:column;
+`
+
+export const TaskCard = styled.ol`
   padding: 0;
   width: 200px;
+  
 `;
-
-
 
 export const TaskForm = styled.div`
   display: grid;
   grid-auto-flow: column;
-  gap: 10px;
 `;
+
 export const Task = styled.li`
   text-align: left;
   text-decoration: ${({ done }) => (done ? "line-through" : "none")};
 `;
+
+export const ButtonAdd = styled.button`
+background:white;
+border:1px solid black;
+&:hover{
+  background:aquamarine;
+}
+`
